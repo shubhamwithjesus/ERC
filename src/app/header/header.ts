4ae32db04e476f69ee44d520c3fd3e96d513c6f3
shubhamwithjesus <shubham.with.jesus@gmail.com>
2025-10-10 12:09:10 +0530

    commented back to top code

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -55,15 +55,15 @@ export class Header implements AfterViewInit {
      */
     if (typeof window !== 'undefined') {
       const header = this.headerElement.nativeElement;
-      const goTopBtn = this.headerElement.nativeElement.querySelector("[data-go-top]");
+      // const goTopBtn = this.headerElement.nativeElement.querySelector("[data-go-top]");
   
       window.addEventListener("scroll", function () {
         if (window.scrollY >= 400) {
           header?.classList.add("active");
-          goTopBtn?.classList.add("active");
+          // goTopBtn?.classList.add("active");
         } else {
           header?.classList.remove("active");
-          goTopBtn?.classList.remove("active");
+          // goTopBtn?.classList.remove("active");
         }
       });
     }
