a11ec1094a51405ff4dbb27ab57b4c7a2e2ba8aa
shubhamwithjesus <shubham.with.jesus@gmail.com>
2025-10-10 10:40:47 +0530

    fixed header

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -10,6 +10,62 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header {
+export class Header implements AfterViewInit {
+  @ViewChild('header') headerElement!: ElementRef;
+
+  ngAfterViewInit(): void {
+    /**
+ * navbar toggle
+ */
+
+    const navOpenBtn = this.headerElement.nativeElement.querySelector("[data-nav-open-btn]");
+    const navbar = this.headerElement.nativeElement.querySelector("[data-navbar]");
+    const navCloseBtn = this.headerElement.nativeElement.querySelector("[data-nav-close-btn]");
+    const overlay = this.headerElement.nativeElement.querySelector("[data-overlay]");
+
+    const elemArr = [navCloseBtn, overlay, navOpenBtn];
+
+    for (let i = 0; i < elemArr.length; i++) {
+      elemArr[i]?.addEventListener("click", function () {
+        navbar?.classList.toggle("active");
+        overlay?.classList.toggle("active");
+      });
+    }
+
+    /**
+     * toggle navbar & overlay when click any navbar-link
+     */
+
+    const navbarLinks = this.headerElement.nativeElement.querySelectorAll("[data-navbar-link]");
+
+    for (let i = 0; i < navbarLinks.length; i++) {
+      navbarLinks[i].addEventListener("click", function () {
+        navbar?.classList.toggle("active");
+        overlay?.classList.toggle("active");
+      });
+    }
+
+
+
+
+
+    /**
+     * header & go-top-btn active
+     * when window scroll down to 400px
+     */
+
+    const header = this.headerElement.nativeElement.querySelector("[data-header]");
+    const goTopBtn = this.headerElement.nativeElement.querySelector("[data-go-top]");
+
+    // window.addEventListener("scroll", function () {
+    //   if (window.scrollY >= 400) {
+    //     header?.classList.add("active");
+    //     goTopBtn?.classList.add("active");
+    //   } else {
+    //     header?.classList.remove("active");
+    //     goTopBtn?.classList.remove("active");
+    //   }
+    // });
+  }
 
 }
