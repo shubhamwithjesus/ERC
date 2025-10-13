5d2ecb3911474da7284ff5351d9aaca899b93ef2
shubhamwithjesus <shubham.with.jesus@gmail.com>
2025-10-13 13:20:35 +0530

    angular based approach for side menu

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,13 +8,16 @@ import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header implements AfterViewInit {
+export class Header {
   @ViewChild('header') headerElement!: ElementRef;
+
   isHomeRoute: boolean = false;
+
+  isNavOpen: boolean = false;
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {
     // Listen to route changes
@@ -23,61 +27,8 @@ export class Header implements AfterViewInit {
         this.isHomeRoute = this.router.url === '/'; // Adjust '/' to your home route path if different
       });
   }
-  
-  ngAfterViewInit(): void {
-    /**
- * navbar toggle
- */
-
-    const navOpenBtn = this.headerElement.nativeElement.querySelector("[data-nav-open-btn]");
-    const navbar = this.headerElement.nativeElement.querySelector("[data-navbar]");
-    const navCloseBtn = this.headerElement.nativeElement.querySelector("[data-nav-close-btn]");
-    const overlay = this.headerElement.nativeElement.querySelector("[data-overlay]");
-
-    const elemArr = [navCloseBtn, overlay, navOpenBtn];
-
-    for (let i = 0; i < elemArr.length; i++) {
-      elemArr[i]?.addEventListener("click", function () {
-        navbar?.classList.toggle("active");
-        overlay?.classList.toggle("active");
-      });
-    }
-
-    /**
-     * toggle navbar & overlay when click any navbar-link
-     */
-
-    const navbarLinks = this.headerElement.nativeElement.querySelectorAll("[data-navbar-link]");
-
-    for (let i = 0; i < navbarLinks.length; i++) {
-      navbarLinks[i].addEventListener("click", function () {
-        navbar?.classList.toggle("active");
-        overlay?.classList.toggle("active");
-      });
-    }
-
-
-
-
-
-    /**
-     * header & go-top-btn active
-     * when window scroll down to 400px
-     */
-    // if (typeof window !== 'undefined') {
-    //   const header = this.headerElement.nativeElement;
-      // const goTopBtn = this.headerElement.nativeElement.querySelector("[data-go-top]");
-  
-      // window.addEventListener("scroll", function () {
-      //   if (window.scrollY >= 400) {
-      //     header?.classList.add("active");
-      //     // goTopBtn?.classList.add("active");
-      //   } else {
-      //     header?.classList.remove("active");
-      //     // goTopBtn?.classList.remove("active");
-      //   }
-      // });
-    // }
+ 
+  toggleNav() {
+    this.isNavOpen = !this.isNavOpen;
   }
-
 }
