7ef23b55a095573a20ab8ac229c0a91537995e5f
shubhamwithjesus <shubham.with.jesus@gmail.com>
2025-10-14 11:21:18 +0530

    testing material card

--- src/app/testimonies/testimonies.ts
+++ src/app/testimonies/testimonies.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
 
 @Component({
   selector: 'app-testimonies',
-  imports: [],
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule],
   templateUrl: './testimonies.html',
   styleUrl: './testimonies.scss'
 })
 export class Testimonies {
+    longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
+  Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
+  usually kept as a companion animal or for showing.`;
 
 }
