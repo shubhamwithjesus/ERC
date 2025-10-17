e06afdef2723ce0f6519b01cca26c508c8fa8512
shubhamwithjesus <shubham.with.jesus@gmail.com>
2025-10-17 12:54:30 +0530

    completed meetings section

--- src/app/meetings/meetings.ts
+++ src/app/meetings/meetings.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Corousel } from '../corousel/corousel';
 
 @Component({
   selector: 'app-meetings',
-  imports: [],
+  imports: [Corousel],
   templateUrl: './meetings.html',
   styleUrl: './meetings.scss'
 })
 export class Meetings {
 
+  meetings = [
+    { title: 'Sunday English Service', time: 'Sunday | 09:30 AM - 10:30 AM IST', location: 'Chapel/Zoom', link: 'https://example.com/sunday-service' },
+    { title: 'Sunday Malayalam Service', time: 'Sunday | 10:30 AM - 12:30 PM IST', location: 'Chapel/Zoom', link: 'https://example.com/sunday-service' },
+    { title: 'GANG youth meeting', time: 'Wednesday | 09:00 PM - 10:30 PM IST', location: 'Zoom', link: 'https://example.com/sunday-service' },
+    { title: 'John & James family prayer', time: 'Friday | 09:00 PM - 10:30 PM IST', location: 'Zoom', link: 'https://example.com/sunday-service' },
+    { title: 'Deborah ladies prayer', time: 'Friday | 03:00 PM - 04:30 PM IST', location: 'Zoom', link: 'https://example.com/sunday-service' },
+  ]
 }
