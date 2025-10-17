33415f6988fd37d52912b576cdce1c98dd914c3c
shubhamwithjesus <shubham.with.jesus@gmail.com>
2025-10-17 11:04:40 +0530

    initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { ConfigService } from './services/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideAppInitializer(() => {
+      const configService = inject(ConfigService);
+      return configService.loadConfig()
+    }),
+    provideHttpClient(withFetch())
   ]
 };
---
+++ src/app/services/config.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+  private config!: any;
+
+  constructor(private http: HttpClient) {}
+
+  loadConfig(): Observable<any> {
+    return this.http.get<any>('./assets/config.json').pipe(
+      tap(data => this.config = data)
+    );
+  }
+
+  get configData(): any {
+    return this.config;
+  }
+}
